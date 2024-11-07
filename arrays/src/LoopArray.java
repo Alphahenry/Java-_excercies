980a5ec1996c19e4f28afb068fa9ef1539b84789
Alphahenry <alphahenry2019@gmail.com>
2024-11-07 18:43:04 +0300

    "looping an array"

---
+++ arrays/src/LoopArray.java
@@ -0,0 +1,10 @@
+//loop through array between 0 and 100 and print out results
+public class LoopArray {
+    public static void main(String[] args) {
+        //declare array
+        int [] age = new int[100];
+        for (int x = 0; x < age.length; x++){
+            System.out.println(x);
+        }
+    }
+}
