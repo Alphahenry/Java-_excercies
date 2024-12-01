7d0d412f306617622c2c32eeea82c7c6e15d5ae8
Alphahenry <alphahenry2019@gmail.com>
2024-12-01 06:25:04 +0300

    first commit

---
+++ Selections/src/Intro.java
@@ -0,0 +1,14 @@
+public class Intro {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 4;
+
+        if (x > 2){
+            if (y >2){
+                int z = x + y;
+                System.out.println("Z is : " + z);
+            }
+        }else
+            System.out.println("x is : " + x);
+    }
+}
