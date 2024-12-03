52b477bcc311ba56a4fc7303457caa043c53052a
Alphahenry <alphahenry2019@gmail.com>
2024-12-03 13:39:43 +0300

    summing two num

---
+++ methods/src/Sum.java
@@ -0,0 +1,15 @@
+//method to sum two integers
+public class Sum {
+    public static void main(String[] args) {
+        int x = 4;
+        int y = 7;
+
+        int res = add(x,y);
+        System.out.println("The sum of the two Is: " + res);
+
+    }
+
+    public  static  int add(int num1, int num2){
+        return num1 + num2;
+    }
+}
