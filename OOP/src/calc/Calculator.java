ea1d40b8de153d46d09c9406de41fe1ba0ad5883
Alphahenry <alphahenry2019@gmail.com>
2025-01-15 22:05:09 +0300

    first commit

---
+++ OOP/src/calc/Calculator.java
@@ -0,0 +1,10 @@
+package calc;
+//simple view of how object is created
+public class Calculator {
+
+    public int add(int num1, int num2){
+        int res = num1 + num2;
+        return res;
+    }
+
+}
---
+++ OOP/src/calc/Main.java
@@ -0,0 +1,16 @@
+package calc;
+
+public class Main {
+    public static void main(String[] args) {
+        int n1 = 4;
+        int n2 = 5;
+
+        //create object calculator
+
+        Calculator calc1 = new Calculator();
+
+        System.out.println("Adding n1 and n2:" + calc1.add(n1, n2));
+
+    }
+
+}
