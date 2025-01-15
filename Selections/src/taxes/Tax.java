8b7a3cb77a6562ec56293198baddb42c8836ef11
Alphahenry <alphahenry2019@gmail.com>
2025-01-15 05:45:32 +0300

    first commit

---
+++ Selections/src/taxes/Tax.java
@@ -0,0 +1,50 @@
+package taxes;
+
+import java.util.Scanner;
+
+// program to calculate Tax for single,married filing jointly,married filing separately and head of family
+
+public class Tax {
+    public static void main(String[] args) {
+
+        //creating scanner object
+        Scanner input = new Scanner(System.in);
+
+        //take inpput from user
+        System.out.println("Single : 0");
+        System.out.println("Married Filing separately: 1");
+        System.out.println("Married filling jointly: 2");
+        System.out.println("Head of family: 3");
+
+        System.out.println("Select Tax bracket: ");
+
+    
+        int Tax_bracket = input.nextInt();
+
+        System.out.println("Enter your income: ");
+        int income = input.nextInt();
+
+        double tax = 0;
+
+        if (Tax_bracket == 0) {
+            if (income <= 8350) {
+                tax = 8350 * 0.1;
+                
+            }
+            else if (income <= 33950) {
+                tax = 8350 * 0.1 + (income - 83950) * 0.15;
+            }
+            else if (income <= 82250) {
+                tax = 8350 * 0.1 + 83950 * 0.15 + (income - 82250) * 0.25;
+                
+            }
+            else{
+                tax = 8350 * 0.1 + 83950 * 0.15 + 82250 * 0.25 + (income - 171000) * 0.33;
+            }
+        }
+
+      System.out.println("Your total tax is : " + tax);
+        
+    }
+
+}
