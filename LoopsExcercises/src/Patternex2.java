4296bc1739da87804954beded8ac937aca007579
Alphahenry <alphahenry2019@gmail.com>
2025-01-16 22:59:37 +0300

    first commit

---
+++ LoopsExcercises/src/Patternex2.java
@@ -0,0 +1,24 @@
+
+
+/*
+print an upside down triangle
+ *****
+ ****
+ ***
+ **
+ *
+
+ */
+
+public class Patternex2 {
+    public static void main(String[] args) {
+               
+        for(int i = 5; i>=1; i--){
+            for(int j= 1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
