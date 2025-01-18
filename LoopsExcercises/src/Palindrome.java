ffea6bf05d4870db3ff215625edd39bd1ffc90da
Alphahenry <alphahenry2019@gmail.com>
2025-01-18 22:27:44 +0300

    first commit

---
+++ LoopsExcercises/src/Palindrome.java
@@ -0,0 +1,42 @@
+
+//program to check if a word is palindrome
+//A palindrome word 
+
+//example of palindrome words is: mom, wow etc
+import java.util.Scanner;
+public class Palindrome {
+    public static void main(String[] args) {
+        //scanner object
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter word: ");
+        String word = input.nextLine();
+
+        //first character index
+        int first_index  = 0;
+
+        //last index
+
+        int last_index = word.length()-1;
+
+        boolean ispalindrome = true;
+        while (first_index < last_index) {
+            if (word.charAt(first_index) != word.charAt(last_index)){
+                ispalindrome = false;
+                break;
+
+               
+            }
+            last_index++;
+            first_index--;
+        }
+        if (ispalindrome) {
+            System.out.println(word + "is a palindrome");
+        }else{
+            System.out.println(word+ "is not  a palindrome");
+        }
+         
+        input.close();
+    }
+    
+}
