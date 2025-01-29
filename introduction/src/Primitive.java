368970145b37313f44633d030451419261b6333e
Alphahenry <alphahenry2019@gmail.com>
2025-01-29 03:49:17 +0300

    first commit

---
+++ introduction/src/Primitive.java
@@ -0,0 +1,32 @@
+//primitive data types include integers,char and boolean
+public class Primitive {
+    public static void main(String[] args) {
+        
+        char letter = 'T';
+        int num = 89;
+        short num2 = 1;
+        long num3 = 4565631324345453l;
+        boolean value = false;
+        float num4 = 79.01f;
+        double num5 = 56.89;
+
+
+        System.out.println("letter" + letter);
+
+        System.out.println("int " + num);
+
+        System.out.println("short" + num2);
+
+        System.out.println("long" + num3);
+
+        System.out.println("boolean" + false);
+
+        System.out.println("float" + num4);
+
+        System.out.println("double" + num5);
+
+
+
+    }
+
+}
