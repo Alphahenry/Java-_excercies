bf469992544385e38f48456a865c2226770d0d39
Alphahenry <alphahenry2019@gmail.com>
2025-02-02 11:38:13 +0300

    fixed gcd error of zero

--- LoopsExcercises/src/Gcd.java
+++ LoopsExcercises/src/Gcd.java
@@ -2,6 +2,19 @@
 //Write a program that prompts the user to enter two
 //positive integers and displays the gcd.
 
+
+/*
+ * 1. a and b be two numbers such that a > b.
+
+  2. Divide the larger number a by, the smaller number b
+3.  Replace ‘a’ with ‘b’ and ‘b’ with the remainder from step-2
+4 . Repeat step-1 and step-2 until the remainder is zero.
+Once you get the remainder 0, the divisor will be the GCD of a and b at this stage.
+ * 
+ * 
+ * 
+ */
+
 import java.util.Scanner;
 public class Gcd {
     public static void main(String[] args) {
@@ -15,31 +28,36 @@ public class Gcd {
     System.out.println("Enter second number: ");
     int n2 = input.nextInt();
 
+    //get the maximum of the two
+    int max_value = Math.max(n1, n2);
+    int min_value = Math.min(n1, n2);
+
+    System.out.println("The large number is: " + max_value );
+    System.out.println("The smaller number is: " + min_value);
+
 
     
 
  //lets get the minimum of the two;
-       int remainder;
-do {
-    if(n1 < n2){
-        remainder = n2 % n1;
-    }
-    else{
-        remainder = n1 % n2;
-    }
 
- 
-      @SuppressWarnings("unused")
-     int temp = n1;
-      n1  = n2;
-      n2 = remainder;  
+ int remainder;
+       
+do {
+    remainder = max_value % min_value;
+
+
+    @SuppressWarnings("unused")
+     int temp = max_value;
+      max_value = min_value;
+      min_value = remainder;  
 
       
 
     
 } while (remainder != 0);
+//when remainder is zero max_value is the gcd since its the last number
 
-System.out.println("GSd is :" + remainder);
+System.out.println("GSd is :" + max_value);
   
     }
 
