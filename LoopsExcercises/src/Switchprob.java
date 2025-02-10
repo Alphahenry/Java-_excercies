06e7f8ccc504f3020bec061daa232aa75e3425f6
Alphahenry <alphahenry2019@gmail.com>
2025-02-10 20:37:44 +0300

    first commit

---
+++ LoopsExcercises/src/Switchprob.java
@@ -0,0 +1,30 @@
+//furnitures and price
+
+import java.util.Scanner;
+public class Switchprob {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //Enter furniture you need
+
+        System.out.println("Enter the furniture: ");
+        String name = input.nextLine();
+        switch (name) {
+            case "Kitchen Table":
+            System.out.println("This is sh 1500 ");
+                break;
+
+            case "Study Table":
+                System.out.println("This is sh 4500");
+                break;
+
+            case "4x6 bed":
+                System.out.println("This is sh 5500 ");
+                break;      
+        
+            default: 
+                break;
+        }
+    }
+
+}
