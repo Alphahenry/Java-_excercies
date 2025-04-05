c060fc334e0970adc0b7955647c758b7a6d158ce
Alphahenry <alphahenry2019@gmail.com>
2025-04-05 20:21:05 +0300

    fist commit

---
+++ Test/src/EnumType.java
@@ -0,0 +1,15 @@
+//how to declare an enum type
+public class EnumType {
+    public static void main(String[] args){
+
+     enum Wear {small,medium, large, extralarge };
+
+      Wear Alpha = Wear.medium;
+
+        if(Alpha == Wear.medium){
+            System.out.println("You are lucky!");
+        }
+
+    }
+
+}
