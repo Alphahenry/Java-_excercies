ec3ca997b44bbbbc55cf81365fd6e2d12a9efc94
Alphahenry <alphahenry2019@gmail.com>
2025-04-11 19:36:52 +0300

    first commit

---
+++ methods/src/intro/Swap.java
@@ -0,0 +1,16 @@
+package intro;
+
+public class Swap {
+    public static void main(String[] args) {
+
+    }
+
+    static  void swap(int a, int b){
+        int temp = a;
+        a = b;
+
+        b = temp;
+        
+
+    }
+}
